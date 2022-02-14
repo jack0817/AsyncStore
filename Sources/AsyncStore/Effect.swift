d4a671a4b45b6e6cc3464c0472cbb167338514d2
jack0817 <jack0817@yahoo.com>
2022-02-14 13:19:52 -0500

    store updates (#1)
    Co-authored-by: Wendell Thompson <wendell.thompson@aboutobjects.com>

---
+++ Sources/AsyncStore/Effect.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wendell Thompson (AO) on 2/14/22.
+//
+
+import Foundation
+
+extension AsyncStore {
+    enum Effect {
+        case none
+        case set(setter: (inout State) -> Void)
+        case task(operation: () async throws -> Effect, id: AnyHashable?)
+        case sleep(TimeInterval)
+        case merge(effects: [Effect])
+        case concatenate(effects: [Effect])
+    }
+}
+
+extension AsyncStore.Effect {
+    static func set(_ setter: @escaping (inout State) -> Void) -> Self {
+        .set(setter: setter)
+    }
+    
+    static func task(
+        _ operation: @escaping () async throws -> Self,
+        _ id: AnyHashable? = .none
+    ) -> Self {
+        .task(operation: operation, id: id)
+    }
+    
+    static func merge(_ effects: Self ...) -> Self {
+        .merge(effects: effects)
+    }
+    
+    static func concatenate(_ effects: Self ...) -> Self {
+        .concatenate(effects: effects)
+    }
+}
