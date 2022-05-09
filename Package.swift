82ac104dd48fa861554a566db9f67689131e26d2
Wendell Thompson <jack0817@yahoo.com>
2022-05-09 14:46:19 -0400

    Created a more accurate AsyncTimer

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AsyncStore",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v12),
         .iOS(.v15),
         .tvOS(.v15)
     ],
--- Sources/AsyncStore/AsyncTimer.swift
+++ Sources/AsyncStore/AsyncTimer.swift
@@ -9,13 +9,13 @@ import Foundation
 
 extension AsyncStore {
     struct AsyncTimer: AsyncSequence {
-        typealias AsyncIterator = AsyncStore.AsyncTimer.Iterator
         typealias Element = Date
+        typealias AsyncIterator = AsyncStore.AsyncTimer.Iterator
         
         let interval: TimeInterval
         
-        func makeAsyncIterator() -> AsyncStore<State, Environment>.AsyncTimer.Iterator {
-            .init(interval: interval)
+        func makeAsyncIterator() -> Iterator {
+            AsyncTimer.Iterator(interval: interval)
         }
     }
 }
@@ -23,11 +23,27 @@ extension AsyncStore {
 extension AsyncStore.AsyncTimer {
     struct Iterator: AsyncIteratorProtocol {
         let interval: TimeInterval
+        private var offsetTime: UInt64 = 0
         
-        mutating func next() async throws -> Date? {
-            try? await Task.trySleep(for: interval)
-            guard !Task.isCancelled else { return .none }
-            return Date()
+        private var nowNano: UInt64 {
+            DispatchTime.now().uptimeNanoseconds
+        }
+        
+        init(interval: TimeInterval) {
+            self.interval = interval
+        }
+        
+        mutating func next() async -> Element? {
+            do {
+                let start = nowNano
+                let sleep = UInt64(interval * 1_000_000_000) - offsetTime
+                try await Task.sleep(nanoseconds: sleep)
+                let elapsed = nowNano - start
+                offsetTime = elapsed > sleep ? elapsed - sleep : 0
+                return .now
+            } catch {
+                return .none
+            }
         }
     }
 }
