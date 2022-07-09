1f758af637238ee7ffcf46ca37d0415be56d0cfd
Wendell Thompson <jack0817@yahoo.com>
2022-07-09 13:25:52 -0400

    100 unit tests AsyncTimer accuracy

--- Tests/AsyncStoreTests/AsyncTimerTests.swift
+++ Tests/AsyncStoreTests/AsyncTimerTests.swift
@@ -13,10 +13,10 @@ final class AsyncTimerTests: XCTestCase {
         let intervalNano: Int64 = 250_000_000
         let timer = AsyncStore<String, String>.AsyncTimer(interval: Double(intervalNano) * 0.000_000_001)
         let accuracyRange: ClosedRange<UInt64> =
-            UInt64(Double(abs(intervalNano)) * 0.9) ... UInt64(Double(abs(intervalNano)) * 1.1)
+            UInt64(Double(abs(intervalNano)) * 0.8) ... UInt64(Double(abs(intervalNano)) * 1.2)
         
         var elapsedTimes: [UInt64] = []
-        let maxCount = 8
+        let maxCount = 2
         var currentCount = 0
         let startTime = DispatchTime.now().uptimeNanoseconds
         var previousTime = startTime
