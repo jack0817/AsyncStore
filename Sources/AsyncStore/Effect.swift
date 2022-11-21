c5a88fa9a12b9c09e763dd1119bf4dd856cfff30
Wendell Thompson <jack0817@yahoo.com>
2022-11-21 15:13:09 -0500

    renamed set API

--- Sources/AsyncStore/Effect.swift
+++ Sources/AsyncStore/Effect.swift
@@ -22,8 +22,8 @@ public extension AsyncStore {
 }
 
 public extension AsyncStore.Effect {
-    static func set<Value>(_ keyPath: WritableKeyPath<State, Value>, to value: Value) -> Self {
-        return .set { $0[keyPath: keyPath] = value }
+    static func set<Value>(_ property: WritableKeyPath<State, Value>, to value: Value) -> Self {
+        return .set { $0[keyPath: property] = value }
     }
     
     static func task(
