ae9db5876869a31c80a7aeed4e2352501b6f5af9
Wendell Thompson <jack0817@yahoo.com>
2026-03-24 14:06:03 -0400

    restart here

--- Package.swift
+++ Package.swift
@@ -18,11 +18,17 @@ let package = Package(
             targets: ["AsyncStore"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AsyncStore"
+            name: "AsyncStore",
+            dependencies: [
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms")
+            ]
         ),
         .testTarget(
             name: "AsyncStoreTests",
