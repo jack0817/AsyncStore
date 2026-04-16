4646e2c26ce2bc18825573a0222908ca324cbb37
Wendell Thompson <jack0817@yahoo.com>
2026-04-16 10:43:38 -0400

    logging, enhance!

--- Sources/AsyncStore/AsyncStore.swift
+++ Sources/AsyncStore/AsyncStore.swift
@@ -56,7 +56,7 @@ public final class AsyncStore<State: Sendable, TaskIdentifier: Hashable & Sendab
         runTask?.cancel()
         tasks.values.forEach { $0.cancel() }
         stateContinuations.values.forEach { $0.finish() }
-        print("[\(type(of: self))] deinit")
+        print("[AsyncStore<\(type(of: State.self)), \(type(of: TaskIdentifier.self))>] deinit")
     }
     
     public subscript<Value>(dynamicMember property: KeyPath<State, Value>) -> Value {
